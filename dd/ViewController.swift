1819d335fec2805b415edc5f1c38c8b29c18e154
Ultimedia <maartenbressinck@gmail.com>
2014-10-13 16:26:26 +0200

    Initial Commit

---
+++ dd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dd
+//
+//  Created by Maarten Bressinck on 13/10/14.
+//  Copyright (c) 2014 Maarten Bressinck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
